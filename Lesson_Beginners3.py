a4a6786d5b5753edc9042ec2bec97063d1237bf3
TheHyBrid <114992994+TheHyBrid-148@users.noreply.github.com>
2023-06-09 17:49:02 +0800

    Updated Lesson_Beginners3.py

---
+++ Lesson_Beginners3.py
@@ -0,0 +1,8 @@
+# This is 3/5 of the full Python's beginners pack.
+# ------------------
+# Section 2 - Functions, Modules, and File Handling
+# ------------------
+
+# Lesson 11 - Functions
+
+# Code 1 - Basic function
