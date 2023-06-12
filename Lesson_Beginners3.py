369b2820fda2d373d9b637b9f37f1bc0b3c731bd
TheHyBrid <114992994+TheHyBrid-148@users.noreply.github.com>
2023-06-12 16:51:42 +0800

    Updated Lesson_Beginners3.py

--- Lesson_Beginners3.py
+++ Lesson_Beginners3.py
@@ -23,3 +23,24 @@ def multiply(a, b):
 
 result = multiply(5, 3)
 print("Result: ", result)
+
+# Code 4 - Recursive function
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n  * factorial(n - 1)
+
+print("Factorial of 5: ", factorial(5))
+
+# Code 5 - Function with default parameter value
+def power(base, exponent=2):
+  return base  ** exponent
+
+print("Power of 3: ", power(3))
+print("Power of 4 with exponent of 3", power(4, 3))
+###################################
+
+# Lesson 12 - Modules and packages
+
+# Code 1 - 
