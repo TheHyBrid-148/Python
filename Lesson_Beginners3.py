6dd14876e7f8aba82c89ac3760de8ffe81aaea9d
TheHyBrid <114992994+TheHyBrid-148@users.noreply.github.com>
2023-06-12 16:46:25 +0800

    Updated Lesson_Beginners3.py

--- Lesson_Beginners3.py
+++ Lesson_Beginners3.py
@@ -1,8 +1,25 @@
 # This is 3/5 of the full Python's beginners pack.
 # ------------------
-# Section 2 - Functions, Modules, and File Handling
+# Section 3 - Functions, Modules, and File Handling
 # ------------------
 
 # Lesson 11 - Functions
 
 # Code 1 - Basic function
+def greet():
+  print("Welcome!")
+
+greet()
+
+# Code 2 - Function with parameters
+def greet(name):
+  print("Hello", + name + "Welcome!")
+
+greet("Alice")
+
+# Code 3 - Function with return value
+def multiply(a, b):
+  return a * b
+
+result = multiply(5, 3)
+print("Result: ", result)
